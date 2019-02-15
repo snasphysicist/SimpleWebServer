b2e7b9ecc35131e919bd8c44dee0aef8b1943601
snasphysicist <ih8coldplay@hotmail.com>
2019-02-15 19:39:25 +0800

    Add TextResponse since it sits more naturally in this package

---
+++ src/com/snasphysicist/SimpleWebServer/TextResponse.java
@@ -0,0 +1,18 @@
+package com.snasphysicist.chinesecharacterpracticesheets;
+
+public class TextResponse extends Response {
+	
+	private String body ;
+	
+	public TextResponse( Protocol protocol, int status, String title, String body ) {
+		super(protocol, status, title) ;
+		this.body = body ; 
+	}
+	
+	@Override
+	protected void generateContent() {
+		generateTopMatter() ;
+		content += "\r\n" + body ;
+	}
+
+}
