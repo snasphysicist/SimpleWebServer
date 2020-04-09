99e165fb18e226ef3101d41e155d8a040a6633e7
snasphysicist <ih8coldplay@hotmail.com>
2020-04-09 21:07:02 +0800

    Improve formatting for TextResponse class file

--- src/com/snasphysicist/simplewebserver/TextResponse.java
+++ src/com/snasphysicist/simplewebserver/TextResponse.java
@@ -1,18 +1,28 @@
-package com.snasphysicist.simplewebserver ;
+package com.snasphysicist.simplewebserver;
 
-public class TextResponse extends Response {
+public class TextResponse 
+	extends Response 
+{
+	private String body;
 	
-	private String body ;
-	
-	public TextResponse( Protocol protocol, int status, String title, String body ) {
-		super(protocol, status, title) ;
-		this.body = body ; 
+	public TextResponse(
+		Protocol protocol,
+		int status,
+		String title,
+		String body
+	) {
+		super(
+			protocol,
+			status,
+			title
+		);
+		this.body = body; 
 	}
 	
 	@Override
-	protected void generateContent() {
-		generateTopMatter() ;
-		content += "\r\n" + body ;
+	protected void generateContent()
+	{
+		generateTopMatter();
+		content += "\r\n" + body;
 	}
-
 }
